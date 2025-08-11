26055d7c18f9866ff9b24c7bf5fb7c1d4a11ba07
cc990304 <983760699@qq.com>
2025-08-11 21:53:02 +0800

    Revert "xiugai"
    This reverts commit c5b2ef22038338c9a1e9bbe2a68c0b90aaec1e32.

--- src/main/java/com/controller/SwaggerConfig.java
+++ src/main/java/com/controller/SwaggerConfig.java
@@ -16,7 +16,6 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        System.out.println("555");
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.controller"))
